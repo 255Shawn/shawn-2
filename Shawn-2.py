ecdf19a692207c5f70a0ca80974ec941a1f827c8
nmschorr <nancyschorr@yahoo.com>
2017-05-13 21:13:35 -0700

    first commit msg. -nancy

---
+++ Shawn-2.py
@@ -0,0 +1,89 @@
+# print "hello world"
+#
+# name = "Shawn"       # this is a comment
+#
+# print name
+#
+# a = "Cat"
+
+# print a
+# a = 2
+# print a
+# b = 3
+# c = a + b
+# print c
+# myval = 5
+# printt = 6
+#
+# a = b = c = 1
+#
+# d = a+b+c
+# print d
+#
+# a,b,c = 1,2,"john"
+
+# print b
+#
+#
+# str = 'Hello World!'
+#
+# print str          # Prints complete string
+# print str[0]       # Prints first character of the string
+# print str[2:5]     # Prints characters starting from 3rd to 5th
+# print str[2:]      # Prints string starting from 3rd character
+# print str * 2      # Prints string two times
+# print str + "TEST" # Prints concatenated string
+
+
+
+
+
+#
+#
+# vary = 101
+#
+# if ( vary  == 101 ) : print "Value of expression is 101"
+# if ( vary  != 99 ) : print "Value of expression is not 99"
+#
+# if ( vary > 2000 ) : print "Value of less than or equal to 200"
+#
+# print "Good bye!"
+#
+
+
+# a = 10
+# b = 20
+# myList = [1, 2, 3, 20, 5];
+#
+# if (a in myList):
+#     print "Line 1 - a is available in the given list"
+# else:
+#     print "Line 1 - a is not available in the given list"
+#     print "I am happy"
+#
+# if (b not in myList):
+#     print "Line 2 - b is not available in the given list"
+# else:
+#     print "Line 2 - b is available in the given list"
+#
+# a = 10
+# if (a in myList):
+#     print "Line 3 - a is available in the given list"
+# else:
+#     print "Line 3 - a is not available in the given list"
+
+
+num = 4
+chat = 8
+while (num <= 9):
+   print 'The count is:', num
+   num = num + 2
+
+   while (chat < 9):
+       print "the value of chat is first " , chat
+       chat = chat + 3
+       print "yes too"
+       print "the value of chat is second " , chat
+print "Good bye!"
+
+
