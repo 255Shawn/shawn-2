8a0efd2fcd5e4e452e69a17222cc3270fc481a77
255shawn <lightcoaching@yahoo.com>
2017-06-04 22:42:03 -0700

    MoreFunctions

--- tuples3.py
+++ tuples3.py
@@ -1,3 +1,4 @@
+from __future__ import division
 # # # # # # tup1 = ('physics', 'chemistry', 1997, 2000);
 # # # # # # tup2 = (1, 2, 3, 4, 5, 6, 7 );
 # # # # #
@@ -85,9 +86,212 @@ import time;  # This is required to include time module.
 # ticks = time.time()
 # print "Number of ticks since 12:00am, January 1, 1970:", ticks
 
-print 0
-localtime = time.localtime(time.time())
-print "Local current time :", localtime
-print 1
+# print 0
+# localtime = time.localtime(time.time())
+# print "Local current time :", localtime
+# print 1
+
+
+
+#
+# # this is setup code: Function definition is here
+# def myPrintFunction( mystr ):
+#    print mystr
+#    mynewvar = mystr[3:7]
+#    return mynewvar
+#
+# # Program starts here    Now you can call printme function
+# newvar = myPrintFunction("aaabbbccc");
+# newvar = myPrintFunction("dddeeefff")
+
+
+# first command: printme (mystr="aaabbbccc")
+# print value of mystr to console
+# all done go to next command
+# 2nd command: printtme (mystr="dddeeefff")
+# print new value of mystr to console
+#
+# # Function definition is here
+# def changeme( intmylist ):
+#    "This changes a passed list into this function"
+#    intmylist.append([1,2,3,4]);
+#    print "Values inside the function: ", intmylist
+#    return
+#
+# # Now you can call changeme function
+# extmylist = [10,20,30];
+# changeme( extmylist );
+# print "Values outside the function: ", extmylist
+#
+#
+#
+
+#
+# # Function definition is here
+# def changeme( intmylist ):
+#    "This changes a passed list into this function"
+#    print 0
+#    intmylist = [1,2,3,4]; # This would assig new reference in mylist
+#    print "Values inside the function: ", intmylist
+#    return
+#
+# def fff():
+#     print 0
+#
+#
+# # Now you can call changeme function
+# mylist = [10,20,30];
+# changeme( mylist );
+# print "Values outside the function: ", mylist
+
+
+
+# # Function definition is here
+# def printme( str ):
+#    "This prints a passed string into this function"
+#    print str
+#    return;
+#
+#
+#
+# # Now you can call printme function
+# printme("HI")
+
+
+# # Function definition is here
+# def printme( intstr ):
+#    "This prints a passed string into this function"
+#    print intstr
+#    # return;
+#
+# # Now you can call printme function
+# printme( intstr = "a string")
+
+# # Function definition is here
+# def printinfo( name, age ):
+#    "This prints a passed info into this function"
+#    print "Name: ", name
+#    print "Age ", age
+#    return;
+#
+# # Now you can call printinfo function
+# printinfo( age=50, name="miki" )
+
+#
+# def printinfo( name, age = 35 ):
+#    "This prints a passed info into this function"
+#    print "Name: ", name
+#    print "Age ", age
+#    return;
+#
+# # Now you can call printinfo function
+# printinfo( age=50, name="miki" )
+# printinfo( name="miki" )
+
+#
+#
+#
+# # Function definition is here
+#
+# def ShawnsFunc  ( name, age = 35 ):
+#    "This prints a passed info into this function"
+#    print "Name: ", name
+#    print "Age ", age
+#    return;
+#
+# # Now you can call printinfo function
+# ShawnsFunc( age=50, name="miki" )
+# ShawnsFunc( name="miki" )
+
+
+
+#
+#
+# myglob = 2
+# nancyIsMyGal = "nancyIsMyGal"
+#
+# def addEmUp( arg1, arg2 ):
+#     # global myglob
+#     myanswer = arg1 + arg2
+#     mydiv = arg1 / arg2
+#     print nancyIsMyGal
+#     print myglob
+#     myMulti = arg1 * arg2
+#     return myanswer, mydiv, myMulti;
+#
+#
+# myNewVar, myNewdiv, myMultiNew = addEmUp(2, 3)
+#
+#
+# print "herez my answer"
+# print "myNewVar equals ", myNewVar
+# print "myNewdiv equals ", myNewdiv
+# print "myMultiNew equals", myMultiNew
+# print "nancy Is My Gal and lives in Santa Clara"
+#
+#
+
+
+
+
+
+
+
+
+#
+#
+# def closet():
+#     towels = 5
+#     washcloth = 6
+#
+#     return towels, washcloth;
+#
+# mytowels, mywashclothes = closet()
+#
+# print "herez whatz in my closet", mytowels,  "towels and", mywashclothes, "washclothes"
+#
+#
+
+
+# again but different:: whatz in garage--how many cars and how many hammers
+
+
+def garage(arg1, arg2):
+    cars = 1, 2, 3
+    hammers = 6, 4, 5
+    return cars[arg1], hammers[arg2];
+
+def garagechange():
+    var1 = 0
+    var2 = 0
+    while var1 < 3:
+        loccar, lochammer = garage(var1, var2)
+        print "What is in garage currently", loccar, "cars and ", lochammer, "hammers"
+        var1 += 1
+        var2 += 1
+
+garagechange()
+
+
+
+
+
 
 print 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
